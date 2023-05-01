ddc71ce51b7a486d3db6b7ab242fbacf2fc57c82
Canoi <canoimaster@gmail.com>
2023-05-01 01:39:46 -0300

    set canvas element size for emscripten;

--- src/window.c
+++ src/window.c
@@ -45,11 +45,13 @@ int l_poti_window_init(lua_State* L) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    emscripten_set_canvas_element_size("#canvas", width, height);
 #else
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
+
 
     _window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, window_flags);
     lua_pushlightuserdata(L, _window);
